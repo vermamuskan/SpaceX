c36b525aec2a2bff1621895260d1e73a84cccbf2
vasuvedant <themuskanverma@gmail.com>
2019-09-28 00:21:28 +0530

    m

---
+++ space_x/Assets/AsteroidManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidManager : MonoBehaviour
+{ 
+    [SerializeField]Asteroid asteroid;
+    [SerializeField]int numberOfAsteroidsOnAnAxis=5;
+    [SerializeField]int gridSpacing=50;
+    void Start(){
+   PlaceAsteroids(); 
+    }
+    void PlaceAsteroids()
+    {
+        for(int x=0;x<numberOfAsteroidsOnAnAxis;x++){
+            for(int y=0;y<numberOfAsteroidsOnAnAxis;y++){
+                for(int z=0;z<numberOfAsteroidsOnAnAxis;z++){
+            InstantiateAsteroid(x,y,z);
+        }
+        }
+        }
+    }
+
+ void InstantiateAsteroid(int x, int y, int z){
+     Instantiate(asteroid,
+     new Vector3( transform.position.x+(x*gridSpacing) + AsteroidOffset(),
+     transform.position.y+(y*gridSpacing)+AsteroidOffset(),
+     transform.position.z+(z*gridSpacing) + AsteroidOffset()),
+      Quaternion.identity,
+      transform);
+ }  
+ float AsteroidOffset(){
+     return Random.Range(-gridSpacing/2f,gridSpacing/2f);
+ }
+
+
+   
+}
