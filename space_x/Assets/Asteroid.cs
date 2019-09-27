14a77162c1705ebaa0b021a261d209ff440958b9
vasuvedant <themuskanverma@gmail.com>
2019-09-27 18:04:24 +0530

    hey

---
+++ space_x/Assets/Asteroid.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+
+    [SerializeField]float minScale=.10f;
+    [SerializeField]float maxScale=1.8f;
+    [SerializeField]float rotationOffset=200f;
+    
+    Transform myT;
+    Vector3 randomRotation;
+
+    void Awake()
+    {
+        myT=transform;
+    }
+    void Start()
+    {
+         Vector3 scale=Vector3.one ;
+         scale.x=Random.Range(minScale,maxScale);
+         scale.y=Random.Range(minScale,maxScale);
+         scale.z=Random.Range(minScale,maxScale);
+         myT.localScale=scale;
+
+         randomRotation.x = Random.Range(-rotationOffset, rotationOffset);
+         randomRotation.y = Random.Range(-rotationOffset, rotationOffset);
+         randomRotation.z = Random.Range(-rotationOffset, rotationOffset);
+         Debug.Log(randomRotation);
+    } 
+    void update()
+    {
+         myT.Rotate(randomRotation * Time.deltaTime);
+    }
+}
+
---
+++ space_x/Assets/play1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play1 : MonoBehaviour
+{
+   [SerializeField]float movementSpeed = 50f;
+   [SerializeField]float turnSpeed = 60f;
+
+    Transform myT;
+
+    void Awake()
+   {
+    myT= transform;
+   }
+    void Update()
+    {
+      Turn();
+      Thrust();
+        
+    }
+   void Turn()
+    {
+        float yaw=turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float pitch=turnSpeed * Time.deltaTime * Input.GetAxis("Pitch");
+        float roll=turnSpeed * Time.deltaTime * Input.GetAxis("Roll");
+        myT.Rotate(pitch,yaw,roll);
+    }   
+    void Thrust()
+    {
+        if(Input.GetAxis("Vertical") > 0)
+      
+        myT.position += myT.forward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+   }
+}
