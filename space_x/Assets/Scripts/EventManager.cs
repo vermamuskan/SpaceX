c400296398401e18a3ba66cd9edd323a717764f3
vasuvedant <themuskanverma@gmail.com>
2019-10-02 00:52:28 +0530

    heyy

---
+++ space_x/Assets/Scripts/EventManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+      public delegate void StartGameDelegate();
+      public static StartGameDelegate onStartGame;
+
+
+
+      public static void Start Game(){
+           if(onStartGame!=null)
+               onStartGame();
+      }
+}
